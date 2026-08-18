67b8263c813d88a70235ac101f48756e526e4367
Smokey Labs <greg.mcever@live.com>
2026-08-17 22:10:00 -0400

    PMG-31 keep deterministic status presenter without unfinished view wiring

--- PitmastersGrill/MainWindow.BoardStatusView.cs
+++ PitmastersGrill/MainWindow.BoardStatusView.cs
@@ -1,16 +1,10 @@
 using PitmastersGrill.Services;
 using System;
-using System.Windows.Controls;
 
 namespace PitmastersGrill
 {
     public partial class MainWindow
     {
         private readonly BoardStatusPresenter _boardStatusPresenter = new(TimeProvider.System);
-
-        private Border BoardStatusFooter => BoardStatusViewControl.FooterBorder;
-        private TextBlock BoardPopulationStatusText => BoardStatusViewControl.PopulationStatusTextBlock;
-        private TextBlock LastRefreshedText => BoardStatusViewControl.LastRefreshedTextBlock;
-        private TextBlock BoardSummaryText => BoardStatusViewControl.SummaryTextBlock;
     }
 }
